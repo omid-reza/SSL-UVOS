0ffa19e23696d9bdea2ef4e85cbc843e460600fe
Omid Reza Heidari <omid.orh@gmail.com>
2024-11-16 10:59:06 -0500

    add job

---
+++ rgb.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -J BETR-ATTN
+#SBATCH --mem=400GB
+#SBATCH --gpus=4
+#SBATCH --mail-type=NONE
+#SBATCH --mail-user=omid.orh@gmail.com
+#SBATCH --partition=phys
+#SBATCH -o ./slurm_outputs/RGB-%j.out
+
+
+module load anaconda
+eval "$(conda shell.bash hook)"
+conda activate /home/o_heidar/bbaenv
+bash start_rgb.sh
