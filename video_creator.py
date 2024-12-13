ba848838d23d3438c0ccc1950904ae7b87187b75
Omid Reza Heidari <omid.orh@gmail.com>
2024-12-12 23:15:00 -0500

    fix format

--- video_creator.py
+++ video_creator.py
@@ -2,7 +2,7 @@ import cv2
 import os
 
 image_folder = 'test_saver'
-video_name = 'video.mp4'
+video_name = 'video.avi'
 
 images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
 frame = cv2.imread(os.path.join(image_folder, images[0]))
