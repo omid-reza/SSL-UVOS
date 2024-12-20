ed71f09a3e37b64274d3abdf641b6125f969fdf2
Omid Reza Heidari <omid.orh@gmail.com>
2024-12-20 00:22:03 -0500

    Update video_creator.py to create video for all of the categories

--- video_creator.py
+++ video_creator.py
@@ -1,18 +1,16 @@
 import cv2
 import os
 
-image_folder = 'test_saver'
-video_name = 'video.avi'
+categories = [x[0] for x in os.walk("Spatio-temporalAttentionMaps")]
 
-images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
-images.sort(key=lambda x: int(x.split('.')[0]))
-frame = cv2.imread(os.path.join(image_folder, images[0]))
-height, width, layers = frame.shape
-
-video = cv2.VideoWriter(video_name, 0, 4, (width,height))
-
-for image in images:
-    video.write(cv2.imread(os.path.join(image_folder, image)))
-
-cv2.destroyAllWindows()
-video.release()
+for category in categories:
+    image_folder = os.path.join("Spatio-temporalAttentionMaps", category)
+    images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
+    images.sort(key=lambda x: int(x.split('.')[0]))
+    frame = cv2.imread(os.path.join(image_folder, images[0]))
+    height, width, layers = frame.shape
+    video = cv2.VideoWriter(os.path.join("Spatio-temporalAttentionMapsVideos", category+".avi"), 0, 4, (width,height))
+    for image in images:
+        video.write(cv2.imread(os.path.join(image_folder, image)))
+    cv2.destroyAllWindows()
+    video.release()
