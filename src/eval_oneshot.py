39f3df27a5c66d2e2afa2d2499aadbd94278ac30
Omid Reza Heidari <omid.orh@gmail.com>
2024-12-22 15:13:57 -0500

    Fix size

--- src/eval_oneshot.py
+++ src/eval_oneshot.py
@@ -208,7 +208,12 @@ def inference(masks_collection, rgbs, gts, model, T, ratio, tau, device, categor
         att_map_avg = averaged_matrix.sum(axis=1) # Shape becomes (H * W)
         print("att_map_avg.shape", att_map_avg.shape)
         att_map_reshaped = att_map_avg.reshape(H, W) # Shape becomes (H, W)
-        plt.imsave(os.path.join(parent_directory, category, f"{t}.png"), att_map_reshaped, cmap='viridis', dpi=300)
+        fig, ax = plt.subplots(figsize=(1920 / 100, 1280 / 100))
+        ax.imshow(att_map_reshaped, cmap='viridis', interpolation='nearest')
+        ax.axis('off')
+        plt.savefig(os.path.join(parent_directory, category, f"{t}.png"), dpi=300, bbox_inches='tight')
+        plt.close(fig)
+        # plt.imsave(os.path.join(parent_directory, category, f"{t}.png"), att_map_reshaped, cmap='viridis', dpi=300)
         print("->SAVED :)")
     #
     ## clustering on the spatio-temporal attention maps and produce segmentation for the whole video
