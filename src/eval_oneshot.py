5990fb584691e8f969fd3f1fce250131d7aa041f
Omid Reza Heidari <omid.orh@gmail.com>
2024-12-22 12:39:59 -0500

    Fix H and W

--- src/eval_oneshot.py
+++ src/eval_oneshot.py
@@ -199,7 +199,7 @@ def inference(masks_collection, rgbs, gts, model, T, ratio, tau, device, categor
     for t in range(attention.shape[0]): # for t in range(number of frames)
         att_map = attention[t].detach().cpu().numpy()
         H = int(np.sqrt(att_map.shape[0]))  # Assuming square spatial dimensions
-        att_map_reshaped = att_map.reshape(H, H)
+        att_map_reshaped = att_map.reshape(40, 60)
         plt.imsave(os.path.join(parent_directory, category, f"{t}.png"), att_map_reshaped, cmap='viridis')
     #
     ## clustering on the spatio-temporal attention maps and produce segmentation for the whole video
