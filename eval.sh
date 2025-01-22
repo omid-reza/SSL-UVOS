19f584a5abdfa6627ce28ca42bf7653b75dddbbc
Omid Reza Heidari <omid.orh@gmail.com>
2025-01-21 20:56:55 -0500

    change commands

--- eval.sh
+++ eval.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #SBATCH -J BBA-EVAL
-#SBATCH --mem=100GB
+#SBATCH --mem=80GB
 #SBATCH --gpus=1
 #SBATCH --mail-type=NONE
 #SBATCH --mail-user=omid.orh@gmail.com
 #SBATCH --partition=all
 #SBATCH -o ./slurm_outputs/EVAL-%j.out
+#SBATCH -w viry4
 
 module load anaconda
 eval "$(conda shell.bash hook)"
