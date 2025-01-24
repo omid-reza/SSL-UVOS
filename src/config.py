d277514e6b448ff6cc7fcc2c46dea028b3231f39
Omid Reza Heidari <omid.orh@gmail.com>
2025-01-23 20:57:51 -0500

    TODO

--- src/config.py
+++ src/config.py
@@ -29,12 +29,11 @@ def setup_dataset(args):
         basepath = args.basepath
         img_dir = basepath + '/JPEGImages/480p'
         gt_dir = basepath + '/Annotations/480p'
-        val_seq = ['dog', 'cows', 'goat', 'camel', 'libby', 'parkour', 'soapbox', 'blackswan', 'bmx-trees',
+        val_seq = ['cows', 'goat', 'camel', 'libby', 'parkour', 'soapbox', 'blackswan', 'bmx-trees',
                    'kite-surf', 'car-shadow', 'breakdance', 'dance-twirl', 'scooter-black', 'drift-chicane',
                    'motocross-jump', 'horsejump-high', 'drift-straight', 'car-roundabout', 'paragliding-launch',
                    'bike-packing', 'dogs-jump', 'gold-fish', 'india', 'judo', 'lab-coat', 'loading', 'mbike-trick',
                    'pigs', 'shooting']
-        val_seq = ['dog']
         val_data_dir = [img_dir, img_dir, gt_dir]
         res = "480p"
 
