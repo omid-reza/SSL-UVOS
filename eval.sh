f0410cbf23d94e29cbef7eb44ba8353443bea5cc
Omid Reza Heidari <omid.orh@gmail.com>
2025-01-23 22:06:35 -0500

    change config

--- eval.sh
+++ eval.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH -J BBA-EVAL
-#SBATCH --mem=260GB
+#SBATCH --mem=370GB
 #SBATCH --gpus=1
 #SBATCH --mail-type=NONE
 #SBATCH --mail-user=omid.orh@gmail.com
 #SBATCH --partition=all
 #SBATCH -o ./slurm_outputs/EVAL-%j.out
-#SBATCH -w virya5
+#SBATCH -w virya4
 
 module load anaconda
 eval "$(conda shell.bash hook)"
