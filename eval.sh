20a8a3c2b9ed17397af497321da8aa4c96963c5d
Omid Reza Heidari <omid.orh@gmail.com>
2025-01-27 10:58:54 -0500

    change config

--- eval.sh
+++ eval.sh
@@ -6,7 +6,7 @@
 #SBATCH --mail-user=omid.orh@gmail.com
 #SBATCH --partition=all
 #SBATCH -o ./slurm_outputs/EVAL-%j.out
-#SBATCH -w virya5
+#SBATCH -w virya6
 
 module load anaconda
 eval "$(conda shell.bash hook)"
