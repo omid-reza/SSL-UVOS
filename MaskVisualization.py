709c72d4e37861bc498005ec3a6af93f1ac88995
Omid Reza Heidari <omid.orh@gmail.com>
2025-01-30 16:32:49 -0500

    Initiat mask visualization

--- MaskVisualization.py
+++ MaskVisualization.py
@@ -2,10 +2,13 @@ import os
 import torch
 from matplotlib import pyplot as plt
 
-masks = ['dog']
+masks = ['dog', 'cows', 'goat', 'camel', 'libby', 'parkour', 'soapbox', 'blackswan', 'bmx-trees',
+                   'kite-surf', 'car-shadow', 'breakdance', 'dance-twirl', 'scooter-black', 'drift-chicane',
+                   'motocross-jump', 'horsejump-high', 'drift-straight', 'car-roundabout', 'paragliding-launch',
+                   'bike-packing', 'dogs-jump', 'gold-fish', 'india', 'judo', 'lab-coat', 'loading', 'mbike-trick',
+                   'pigs', 'shooting']
 for mask in masks:
-    masks_collection = torch.load('masks/{}.pth'.format(mask))
-    # print(masks_collection.keys())
+    masks_collection = torch.load('DAVIS_Attn/{}.pth'.format(mask))
     for fr_idx in masks_collection.keys():
         frame = masks_collection[fr_idx][0][0]
         frame = frame.cpu().detach().numpy()
