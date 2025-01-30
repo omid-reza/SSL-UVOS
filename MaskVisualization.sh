a90238030871b9c3e3a95438039c2120896d348e
Omid Reza Heidari <omid.orh@gmail.com>
2025-01-30 16:37:46 -0500

    change node and mem

--- MaskVisualization.sh
+++ MaskVisualization.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH -J BBA-Mask
-#SBATCH --mem=130GB
+#SBATCH --mem=80GB
 #SBATCH --gpus=1
 #SBATCH --mail-type=NONE
 #SBATCH --mail-user=omid.orh@gmail.com
-#SBATCH --partition=phys
+#SBATCH --partition=all
 #SBATCH -o ./slurm_outputs/MaskVisualization-%j.out
-#SBATCH -w virya2
+#SBATCH -w virya5
 
 module load anaconda
 eval "$(conda shell.bash hook)"
