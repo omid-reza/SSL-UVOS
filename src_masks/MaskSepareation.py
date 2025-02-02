58f310e71ebd0d01da5fc06b6767f5cf0fdedb58
Omid Reza Heidari <omid.orh@gmail.com>
2025-02-02 14:46:49 -0500

    create folder sor codes that belong to mask

--- src_masks/MaskSepareation.py
+++ src_masks/MaskSepareation.py
@@ -8,13 +8,13 @@ masks = ['dog', 'cows', 'goat', 'camel', 'libby', 'parkour', 'soapbox', 'blacksw
                    'bike-packing', 'dogs-jump', 'gold-fish', 'india', 'judo', 'lab-coat', 'loading', 'mbike-trick',
                    'pigs', 'shooting']
 for mask in masks:
-    masks_collection = torch.load('DAVIS_Attn/{}.pth'.format(mask))
+    masks_collection = torch.load('../DAVIS_Attn/{}.pth'.format(mask))
     for fr_idx in masks_collection.keys():
         frame = masks_collection[fr_idx][0][0]
         frame = frame.cpu().detach().numpy()
         for layer_idx, layer in enumerate(frame):
             layer = layer[0]
             print("Layer {}:".format(layer_idx))
-            save_dir = 'masks/{}_split/{}/'.format(mask, layer_idx)
+            save_dir = '../masks/{}_split/{}/'.format(mask, layer_idx)
             os.makedirs(save_dir, exist_ok=True)
-            plt.imsave('masks/{}_split/{}/{}.png'.format(mask, layer_idx, fr_idx), layer)
+            plt.imsave('../masks/{}_split/{}/{}.png'.format(mask, layer_idx, fr_idx), layer)
