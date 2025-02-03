856b2139bb53d5c74f7733c1705fc05f0a78e4e1
Omid Reza <omid.orh@gmail.com>
2025-02-02 20:37:18 -0500

    add more jobs and new task

---
+++ jobs/MaskObjectSelection.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH -J BBAMObjS
+#SBATCH --mem=80GB
+#SBATCH --gpus=1
+#SBATCH --mail-type=NONE
+#SBATCH --mail-user=omid.orh@gmail.com
+#SBATCH --partition=all
+#SBATCH -o ./slurm_outputs/MaskSeparation-%j.out
+#SBATCH --chdir=../
+#SBATCH -w virya5
+
+module load anaconda
+eval "$(conda shell.bash hook)"
+conda activate /home/o_heidar/bbaenv
+python src_masks/MaskObjectSelection.py
---
+++ src_masks/MaskObjectSelection.py
@@ -0,0 +1,1 @@
+print("Worksssssssssssssssssssss :0")
